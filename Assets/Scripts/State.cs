9ab90d39b0504beeda5bbb922851ca3bd2cb9a01
Roger Wang <rogerwang2520@gmail.com>
2021-06-19 21:46:56 -0700

    0.6.0
    Added state and state machine abstract classes

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MEC;
+
+// Source: https://www.youtube.com/watch?v=G1bd75R10m4
+public abstract class State  // Derive with a constructor with a system parameter (e.g. abstract class BattleState with BattleSystem field)
+{
+    public virtual IEnumerator<float> OnStateEnter()
+    {
+        yield return Timing.WaitForOneFrame;
+    }
+
+    public virtual IEnumerator<float> OnStateExit()
+    {
+        yield return Timing.WaitForOneFrame;
+    }
+}
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using MEC;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    protected State state;
+
+    public void SetState(State state)
+    {
+        Timing.RunCoroutine(state.OnStateExit());
+        this.state = state;
+        Timing.RunCoroutine(state.OnStateEnter());
+    }
+}
